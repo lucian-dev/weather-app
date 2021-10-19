b4cca2c45fa3ffbf534af7ba9720646e7785b330
lucian-dev <lucian.dev13@gmail.com>
2021-10-19 15:22:44 +0300

    #sm

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ require("dotenv").config();
 module.exports = {
   reactStrictMode: false,
   target: "serverless",
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     domains: ["openweathermap.org"],
   },
